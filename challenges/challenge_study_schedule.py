bade770e58fa1a85309bc1ab24675e7505f218ca
DevPBDias <devpbdias@gmail.com>
2023-01-27 16:30:50 -0300

    fix: reducing complexity of study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,5 +5,5 @@ def study_schedule(permanence_period, target_time):
             if first_hour <= target_time <= last_hour:
                 count += 1
         return count
-    except TypeError or ValueError:
+    except TypeError:
         return None
